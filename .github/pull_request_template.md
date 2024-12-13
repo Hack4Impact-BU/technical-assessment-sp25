dd68e67550b31a9bb8141379bc8830002db1f01f
Jimmy Hou <jhou22@bu.edu>
2024-12-12 21:21:29 -0500

    completion

--- .github/pull_request_template.md
+++ .github/pull_request_template.md
@@ -1,26 +1,26 @@
-# < YOUR FULL NAME >'s Pull Request
-**Date**: < date here>
+# Jimmy Hou's Pull Request
+**Date**: 12/12/2024
 
-**BU email**: < email here>
+**BU email**: jhou22@bu.edu
 
-**Class Year**: < year here>
+**Class Year**: 2026
 
-**Favorite Hobby**: < hobby here>
+**Favorite Hobby**: CODING
 
 ## Requirements
 Please check all of these that you completed
 
-- [ ] Query the Genius [api endpoint](https://docs.genius.com/#/getting-started-h1) and display 3 random songs in cards
-- [ ] Have a backend to store the comments you get everyday and the songs that day 
-- [ ] Have function to be able to go back to previous days to see the songs/discussion then
+- [x] Query the Genius [api endpoint](https://docs.genius.com/#/getting-started-h1) and display 3 random songs in cards
+- [x] Have a backend to store the comments you get everyday and the songs that day 
+- [x] Have function to be able to go back to previous days to see the songs/discussion then
 
 ## Bonuses
 Please check all of these that you completed
 
 - [ ] Deploy your application (frontend, backend, and database) so that it is able to be interacted with online
   - **Link**: < paste it here>
-- [ ] If someone makes a comment using the same name multiple times, put a checkmark next to their name showing they contribute a lot
-- [ ] Add a "vote" function to the form where they add a comment for which of the 3 they liked the most. At the end of the day (according to Coordinated Universal Time -- or UTC) identify a song as the "winner" and show that it won if people ever go back to check previous days
+- [x] If someone makes a comment using the same name multiple times, put a checkmark next to their name showing they contribute a lot
+- [x] Add a "vote" function to the form where they add a comment for which of the 3 they liked the most. At the end of the day (according to Coordinated Universal Time -- or UTC) identify a song as the "winner" and show that it won if people ever go back to check previous days
 
 ## Screen Recording 
 **Link**: < paste it here>
@@ -28,7 +28,7 @@ Please check all of these that you completed
 ## Written Portion
 1. **How did the project go? What parts of it did you most enjoy / find yourself good at?**
    
-< answer here>
+This project was overall pretty interesting to make
 
 2. **What would you change about this assessment for next year?**
    
--- backend/app.js
+++ backend/app.js
@@ -1,58 +1,148 @@
-import express from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import dotenv from 'dotenv'
-import { MongoClient } from 'mongodb'
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
-const mongoUrl = process.env.MONGODB_CONNECT_URL
-const mongo = new MongoClient(mongoUrl, {})
+const mongoUrl = process.env.MONGODB_CONNECT_URL;
+const mongo = new MongoClient(mongoUrl, {});
 
 mongo.connect().then(() => {
-    console.log("Connected to MongoDB")
-})
+  console.log("Connected to MongoDB");
+});
+
+const database = mongo.db("music");
+const discussionData = database.collection("songs");
 
 function getRandomInt(min, max) {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
-  
-app.get('/songs', (req, res) => {
-    console.log("GETTING SONGS")
-    let songs = [];
-    for (let i = 0; i < 3; i++) {
-        // 8500000
-        // 8061175
-        const num = getRandomInt(8061177, 8054000)
-            fetch(`https://api.genius.com/songs/${num}`, {
-                method: "GET",
-                "headers": {
-                    "Authorization": `Bearer ${process.env.GENIUS_ACCESS_TOKEN}`
-                }
-            }
-        )
-            .then((data) => data.json())
-            .then((data) => {
-                // console.log(data)
-                // songs.push({'artist': data["artist_names"], "link": data["apple_music_player_url"], "cover_image": data["header_image_url"]})
-                console.log(num)
-                console.log(data.response)
-            })
-        
+
+app.post("/songs", async (req, res) => {
+  const aggregatedData = await discussionData
+    .aggregate([
+      { $unwind: "$comments" },
+      { $project: { name: { $toLower: "$comments.name"} } },
+      { $group: { _id: "$name", count: { $sum: 1 } } },
+      { $match: { count: { $gt: 1 } } },
+      { $project: { _id: 0, name: "$_id" } },
+    ])
+    .toArray();
+  const date = req.body.date;
+  const inputDate = new Date(date);
+  const today = new Date();
+  let songs = [];
+
+  const query = { date: date };
+  const options = {
+    projection: { _id: 0 },
+  };
+  const result = await discussionData.findOne(query, options);
+  if (result) {
+    return res.json({
+      success: true,
+      result: result,
+      frequentCommenters: aggregatedData
+    });
+  }
+  // didn't find any data, so check if date is today's date, if so create a new document in the db, else return nothing
+  if (inputDate.setHours(0, 0, 0, 0) != today.setHours(0, 0, 0, 0)) {
+    return res.json({
+      success: false,
+    });
+  }
+  // is today's date, so create a new document in mongo and return it
+  while (songs.length < 3) { // grab 3 songs
+    const num = getRandomInt(7393000, 10024527);
+    const response = await fetch(`https://api.genius.com/songs/${num}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.GENIUS_ACCESS_TOKEN}`,
+      },
+    });
+    if (response.ok) {
+      const res = await response.json();
+      songs.push({
+        title: res["response"]["song"].title,
+        artist: res["response"]["song"].artist_names,
+        link: res["response"]["song"].apple_music_player_url,
+        cover: res["response"]["song"].header_image_url,
+      });
     }
-    res.json({
-        songData: songs,
-    })
-})
+  }
+  const doc = {
+    date: date,
+    songs: songs,
+    comments: [],
+    votes: [0, 0, 0],
+    favorite_song: "",
+  };
+  await discussionData.insertOne(doc);
+  const data = await discussionData.findOne(query, options);
+  res.json({
+    success: true,
+    result: data,
+    frequentCommenters: aggregatedData
+  });
+});
+
+app.post("/update_comments", async (req, res) => {
+  const comment = req.body.comment;
+  const date = req.body.date;
+  const songVoted = req.body.favorite_song; // song index number in database
+  const filter = { date: date };
+  const options = {
+    projection: { _id: 0, date: 0, songs: 0 },
+  };
+  let updateDoc = {};
+  if (songVoted || songVoted == 0) { // 0 counts as false so must include this 0 condition
+    updateDoc = {
+      $push: {
+        comments: comment,
+      },
+      $inc: {
+        [`votes.${songVoted}`]: 1,
+      },
+    };
+  } else {
+    updateDoc = {
+      $push: {
+        comments: comment,
+      },
+    };
+  }
+  await discussionData.updateOne(filter, updateDoc, options); // push comment into database and increment vote if needed
+  const query = { date: date };
+  const option = {
+    projection: { _id: 0 },
+  };
+  const result = await discussionData.findOne(query, option);
+  const aggregatedData = await discussionData
+    .aggregate([
+      { $unwind: "$comments" },
+      { $project: { name: { $toLower: "$comments.name"} } },
+      { $group: { _id: "$name", count: { $sum: 1 } } },
+      { $match: { count: { $gt: 1 } } },
+      { $project: { _id: 0, name: "$_id" } },
+    ])
+    .toArray();
+  res.json({
+    success: true,
+    comments: result,
+    frequentCommenters: aggregatedData
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Listening at port ${PORT}`)
-})
+  console.log(`Listening at port ${PORT}`);
+});
